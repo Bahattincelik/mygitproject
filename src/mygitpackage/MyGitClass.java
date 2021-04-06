533c571c09bfdc870c4ee5656b30fbb5f19b6ede
Bahattincelik <75189249+Bahattincelik@users.noreply.github.com>
2021-04-07 00:48:50 +0300

    Update MyGitClass.java
    Github.com daki yeni versiyonumu olusturuyorum

--- src/mygitpackage/MyGitClass.java
+++ src/mygitpackage/MyGitClass.java
@@ -8,6 +8,7 @@ public class MyGitClass {
 		System.out.println("Version 1");
 		System.out.println("My second commit");
 		System.out.println("My third commit");
+		System.out.println("Uzak sunucudaki kodum");
 
 
 	}
